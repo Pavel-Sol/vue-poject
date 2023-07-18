e1ca2f4f045973a43db7ad03668193b7f1e49e70
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2023-07-18 13:50:11 +0400

    анимация списка

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,9 @@
 <template>
   <div v-show="posts.length > 0">
     <h3>список постов</h3>
-    <PostItem v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)" />
+    <transition-group name="post-list">
+      <PostItem v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)" />
+    </transition-group>
   </div>
 
   <h2 v-show="posts.length === 0" style="color: red">постов пока нет</h2>
@@ -23,3 +25,23 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+.post-list-move {
+  transition: transform 0.8s ease;
+}
+</style>
